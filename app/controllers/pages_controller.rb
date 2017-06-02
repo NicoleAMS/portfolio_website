bcaccbd13b7aa16d886d7d1fa652f6c436237486
NicoleS <hjjsentis@MacBook-NicoleS.local>
2017-06-02 17:26:09 +0100

    download cv functionality implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
 
   def home 
   end 
+
+  def pdf
+    send_file Rails.root.join('files', 'Nicole_Sentis_CV.pdf'), :type=>"application/pdf", :x_sendfile=>true 
+  end 
   
 end
